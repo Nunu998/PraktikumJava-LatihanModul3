596be2364d7ce4d635a8ea6941a2e65a84918e3f
Nunu998 <84434671+Nunu998@users.noreply.github.com>
2021-06-13 18:14:37 +0700

    Menambahkan Event Listener

--- src/GuiByText.java
+++ src/GuiByText.java
@@ -6,7 +6,7 @@ public class GuiByText extends JFrame {
 
     JLabel label1 = new JLabel("Coba Input Text");
     JTextField textField1 = new JTextField();
-    JButton button1 = new JButton("Greet");
+    JButton button1 = new JButton("Salam");
 
     GuiByText(){
         setSize(640,480);
